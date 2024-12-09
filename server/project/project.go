5736cf2fbb7337ddacfad6b862cfa0050b8ff351
Johann Homonnai <j.homonnai@icloud.com>
2024-12-09 11:19:17 -0300

    ref(Project): add timestamps

--- server/project/project.go
+++ server/project/project.go
@@ -1,22 +1,29 @@
 package project
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 )
 
 // A Project is a collection of tasks. The only attribute releant to the user is
 // its name.
 type Project struct {
-	Name  string      // Name of the project
-	Tasks []uuid.UUID // IDs of the project's tasks
-	ID    uuid.UUID   // ID of the project
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Name      string      // Name of the project
+	Tasks     []uuid.UUID // IDs of the project's tasks
+	ID        uuid.UUID   // ID of the project
 }
 
 // Create a new instance of a project.
 func NewProject(name string) Project {
+	now := time.Now()
 	return Project{
-		ID:    uuid.New(),
-		Name:  name,
-		Tasks: []uuid.UUID{},
+		ID:        uuid.New(),
+		Name:      name,
+		Tasks:     []uuid.UUID{},
+		CreatedAt: now,
+		UpdatedAt: now,
 	}
 }
