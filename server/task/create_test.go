ce15e314485b99b23c9a719b637d8ffa45587fae
Johann Homonnai <j.homonnai@icloud.com>
2024-12-09 11:44:56 -0300

    ref(task/create_test): lint testify usage

--- server/task/create_test.go
+++ server/task/create_test.go
@@ -6,41 +6,42 @@ import (
 	"github.com/google/uuid"
 	"github.com/murasakiwano/todoctian/server/project"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestCreateTask_Success(t *testing.T) {
 	taskService, projectService := setupServices()
 
 	testProject, err := projectService.CreateProject("My test project")
-	assert.Nil(t, err, "expected project creation to succeed, but got error: %v", err)
+	require.NoError(t, err, "expected project creation to succeed, but got error: %v", err)
 
 	_, err = taskService.CreateTask("My test task", testProject.ID, nil)
-	assert.Nil(t, err, "expected task creation to succeed, but got error: %v", err)
+	require.NoError(t, err, "expected task creation to succeed, but got error: %v", err)
 }
 
 func TestCreateTask_UpdatesParentTask(t *testing.T) {
 	taskService, projectService := setupServices()
 
 	testProject, err := projectService.CreateProject("My test project")
-	assert.Nil(t, err, "expected project creation to succeed, but got error: %v", err)
+	require.NoError(t, err, "expected project creation to succeed, but got error: %v", err)
 
 	task, err := taskService.CreateTask("My test task", testProject.ID, nil)
-	assert.Nil(t, err, "expected task creation to succeed, but got error: %v", err)
+	require.NoError(t, err, "expected task creation to succeed, but got error: %v", err)
 
 	subtask, err := taskService.CreateTask("My subtask", testProject.ID, &task.ID)
-	assert.Nil(t, err, "expected subtask creation to succeed, but it failed: %v", err)
+	require.NoError(t, err, "expected subtask creation to succeed, but it failed: %v", err)
 
 	task, err = taskService.taskDB.Get(task.ID)
-	assert.Nil(t, err)
+	require.NoError(t, err)
 
 	assert.NotEmpty(t, task.Subtasks, "adding a subtask to a task did not successfully update the parent task")
 	assert.Equal(t, task.Subtasks[0].ID, subtask.ID, "adding a subtask to a task did not successfully update the parent task")
 
 	subtask, err = taskService.CreateTask("My second subtask", testProject.ID, &task.ID)
-	assert.Nil(t, err)
+	require.NoError(t, err)
 
 	task, err = taskService.taskDB.Get(task.ID)
-	assert.Nil(t, err)
+	require.NoError(t, err)
 
 	assert.NotEmpty(t, task.Subtasks, "adding a subtask to a task did not successfully update the parent task")
 	assert.Equal(t, task.Subtasks[1].ID, subtask.ID, "adding a subtask to a task did not successfully update the parent task")
@@ -50,28 +51,28 @@ func TestCreateTask_ProjectDoesNotExist(t *testing.T) {
 	taskService := NewTaskService(NewTaskRepositoryInMemory(), project.NewProjectRepositoryInMemory())
 
 	_, err := taskService.CreateTask("My test task", uuid.New(), nil)
-	assert.NotNil(t, err, "expected task creation without an existing project to fail, which didn't happen.")
+	assert.Error(t, err, "expected task creation without an existing project to fail, which didn't happen.")
 }
 
 func TestCreateTask_ParentTaskIsInvalid(t *testing.T) {
 	taskService, projectService := setupServices()
 
 	testProject, err := projectService.CreateProject("My test project")
-	assert.Nil(t, err, "expected project creation to succeed, but got error: %v", err)
+	require.NoError(t, err, "expected project creation to succeed, but got error: %v", err)
 
 	parentID := uuid.New()
 	_, err = taskService.CreateTask("My test task", testProject.ID, &parentID)
-	assert.NotNil(t, err, "expected task creation with an invalid parent task to fail")
+	assert.Error(t, err, "expected task creation with an invalid parent task to fail")
 }
 
 func TestCreateTask_SetsOrderCorrectly(t *testing.T) {
 	taskService, projectService := setupServices()
 
 	project, err := projectService.CreateProject("Test Project")
-	assert.Nil(t, err)
+	require.NoError(t, err)
 
 	task, err := taskService.CreateTask("First task", project.ID, nil)
-	assert.Nil(t, err)
+	require.NoError(t, err)
 
 	assert.Equal(
 		t, 0, task.Order,
@@ -79,7 +80,7 @@ func TestCreateTask_SetsOrderCorrectly(t *testing.T) {
 	)
 
 	task, err = taskService.CreateTask("Second task", project.ID, nil)
-	assert.Nil(t, err)
+	require.NoError(t, err)
 
 	assert.Equal(
 		t, 1, task.Order,
@@ -91,10 +92,10 @@ func TestCreateTask_SubtaskDoesNotAffectParentTaskOrder(t *testing.T) {
 	taskService, projectService := setupServices()
 
 	project, err := projectService.CreateProject("Test Project")
-	assert.Nil(t, err)
+	require.NoError(t, err)
 
 	parentTask, err := taskService.CreateTask("Parent task", project.ID, nil)
-	assert.Nil(t, err)
+	require.NoError(t, err)
 
 	assert.Equal(
 		t, 0, parentTask.Order,
@@ -102,7 +103,7 @@ func TestCreateTask_SubtaskDoesNotAffectParentTaskOrder(t *testing.T) {
 	)
 
 	subtask, err := taskService.CreateTask("Subtask", project.ID, &parentTask.ID)
-	assert.Nil(t, err)
+	require.NoError(t, err)
 
 	assert.Equal(
 		t, 0, subtask.Order,
@@ -110,7 +111,7 @@ func TestCreateTask_SubtaskDoesNotAffectParentTaskOrder(t *testing.T) {
 	)
 
 	parentTask, err = taskService.taskDB.Get(parentTask.ID)
-	assert.Nil(t, err)
+	require.NoError(t, err)
 
 	assert.Equal(
 		t, 0, parentTask.Order,
